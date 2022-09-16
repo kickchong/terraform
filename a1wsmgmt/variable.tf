00d5a9afb628b2bcde562435dd50ac4003ff3374
kickchong <kickchong@gmail.com>
2022-09-16 15:16:49 -0700

    a d

--- a1wsmgmt/variable.tf
+++
@@ -1,9 +0,0 @@
-# variable "a1_aws_region" {
-#   description = "AWS Region"
-#   type        = string
-# }
-
-# variable "environment" {
-#   description = "Environment Variable for example dev02, stage02, prod02"
-#   type        = string
-# }
