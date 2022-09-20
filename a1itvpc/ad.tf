8def3dd7ef73679c10a10fc71367fa1d3496b49f
kickchong <kickchong@gmail.com>
2022-09-20 13:00:11 -0700

    add ad

--- a1itvpc/ad.tf
+++ a1itvpc/ad.tf
@@ -1,23 +1,20 @@
-resource "aws_directory_service_directory" "sdc" {
+resource "aws_directory_service_directory" "a1" {
   name        = var.domain_name
   password    = var.domain_password
-  type        = "MicrosoftAD"
+  type        = "SimpleAD"
+  size        = "Small"
   description = "Managed by terraform.io"
-
   vpc_settings {
-    vpc_id     = data.aws_vpc.selected.id
-    subnet_ids = flatten([data.aws_subnet_ids.private.ids])
-  }
-
-  tags = {
-    Terraform = "True"
+    vpc_id     = aws_vpc.v21qw1.id
+  #  subnet_ids = flatten([aws_subnet_ids.private.ids])
+    subnet_ids = flatten([aws_subnet.westwebsub[0].id])
   }
 }
 
 #NTP Servers are pointing to US National Institute of Standards and Technology: time-a-g.nist.gov
 resource "aws_vpc_dhcp_options" "this" {
   domain_name          = var.domain_name
-  domain_name_servers  = aws_directory_service_directory.sdc.dns_ip_addresses
+  domain_name_servers  = aws_directory_service_directory.a1.dns_ip_addresses
   ntp_servers          = ["129.6.15.28","129.6.15.29"]
 
   tags = {
@@ -27,22 +24,12 @@ resource "aws_vpc_dhcp_options" "this" {
 }
 
 resource "aws_vpc_dhcp_options_association" "dns_resolver" {
-  vpc_id          = data.aws_vpc.selected.id
+  vpc_id          = aws_vpc.v21qw1.id
   dhcp_options_id = aws_vpc_dhcp_options.this.id
 }
 
-resource "aws_directory_service_conditional_forwarder" "aws_local" {
-  directory_id        = aws_directory_service_directory.sdc.id
-  remote_domain_name  = "aws.local"
-
-  dns_ips = [
-    "10.159.49.28",
-    "10.159.75.138",
-  ]
-}
-
-resource "aws_directory_service_conditional_forwarder" "qa_aws_local" {
-  directory_id        = aws_directory_service_directory.sdc.id
+resource "aws_directory_service_conditional_forwarder" "a1_aws_local" {
+  directory_id        = aws_directory_service_directory.a1.id
   remote_domain_name  = var.dnszone
 
   dns_ips = [
@@ -50,33 +37,13 @@ resource "aws_directory_service_conditional_forwarder" "qa_aws_local" {
   ]
 }
 
-resource "aws_directory_service_conditional_forwarder" "efs_uswest1_amazonaws_com" {
-  directory_id        = aws_directory_service_directory.sdc.id
-  remote_domain_name  = "efs.us-west-1.amazonaws.com"
-
-  dns_ips = [
-    var.condfwdip,
-  ]
-}
-
-resource "aws_directory_service_conditional_forwarder" "corp_stamps_com" {
-  directory_id        = aws_directory_service_directory.sdc.id
-  remote_domain_name  = "corp.stamps.com"
-
-  dns_ips = [
-    "10.159.49.28",
-    "10.159.75.138"
-  ]
-}
-
+## Create AD Management server
 resource "aws_instance" "admgmt" {
     ami                    = "ami-0b7c10374cfb013e6"
     instance_type          = "m5.large"
     key_name               =  "Stamps-AWS-KP"
-    subnet_id              = data.aws_ssm_parameter.private_subnet.value
-    vpc_security_group_ids = data.aws_security_groups.admgmt.ids
-
-
+    subnet_id              = aws_subnet.westwebsub[0].id
+    vpc_security_group_ids = aws_security_groups.admgmt.ids
     tags = {
       "Name"        = "admgmt"
       "Description" = "Managed by terraform.io"
@@ -89,23 +56,55 @@ resource "aws_instance" "admgmt" {
     EOF
 }
 
-resource "aws_instance" "pwdmgmt01" {
-    count                  = var.environment == "stamps01qa99" ? 1 : 0
-    ami                    = "ami-0b7c10374cfb013e6"
-    instance_type          = "m5.large"
-    key_name               =  "Stamps-AWS-KP"
-    subnet_id              = data.aws_ssm_parameter.private_subnet.value
-    vpc_security_group_ids = data.aws_security_groups.pwdmgmt[0].ids
+# resource "aws_instance" "pwdmgmt01" {
+#     count                  = var.environment == "stamps01qa99" ? 1 : 0
+#     ami                    = "ami-0b7c10374cfb013e6"
+#     instance_type          = "m5.large"
+#     key_name               =  "Stamps-AWS-KP"
+#     subnet_id              = data.aws_ssm_parameter.private_subnet.value
+#     vpc_security_group_ids = data.aws_security_groups.pwdmgmt[0].ids
 
 
-    tags = {
-      "Name"        = "pwdmgmt01"
-      "Description" = "Managed by terraform.io"
-    }
+#     tags = {
+#       "Name"        = "pwdmgmt01"
+#       "Description" = "Managed by terraform.io"
+#     }
 
-    user_data = <<EOF
-    <powershell>
-    Add-Computer -DomainName ${var.domain_name} -NewName "pwdmgmt01" -Credential (New-Object -TypeName PSCredential -ArgumentList "admin",(ConvertTo-SecureString -String ${var.domain_password} -AsPlainText -Force)[0]) -Restart
-    </powershell>
-    EOF
-}
+#     user_data = <<EOF
+#     <powershell>
+#     Add-Computer -DomainName ${var.domain_name} -NewName "pwdmgmt01" -Credential (New-Object -TypeName PSCredential -ArgumentList "admin",(ConvertTo-SecureString -String ${var.domain_password} -AsPlainText -Force)[0]) -Restart
+#     </powershell>
+#     EOF
+# }
+
+# resource "aws_directory_service_conditional_forwarder" "aws_local" {
+#   directory_id        = aws_directory_service_directory.a1.id
+#   remote_domain_name  = "aws.local"
+
+#   dns_ips = [
+#     "10.159.49.28",
+#     "10.159.75.138",
+#   ]
+# }
+
+
+# resource "aws_directory_service_conditional_forwarder" "efs_uswest1_amazonaws_com" {
+#   directory_id        = aws_directory_service_directory.sdc.id
+#   remote_domain_name  = "efs.us-west-1.amazonaws.com"
+
+#   dns_ips = [
+#     var.condfwdip,
+#   ]
+# }
+
+# resource "aws_directory_service_conditional_forwarder" "corp_stamps_com" {
+#   directory_id        = aws_directory_service_directory.sdc.id
+#   remote_domain_name  = "corp.stamps.com"
+
+#   dns_ips = [
+#     "10.159.49.28",
+#     "10.159.75.138"
+#   ]
+# }
+
+
---
+++ a1itvpc/sg.tf
@@ -0,0 +1,76 @@
+## SG for admgmt
+resource "aws_security_group" "admgmt" {
+  name        = "a1-admgmt-sg"
+  description = "SG for use with admgmt"
+  vpc_id      = aws_vpc.a1.id
+  tags = {
+    Owner = "terraform.io"
+    Name  = "a1-admgmt-sg"
+  }
+}
+
+variable "admgmt_ingress_ports" {
+  description = "Allowed ports on admgmt"
+  default     = {
+    "445"  = ["10.64.0.0/16"]
+    "3389" = ["10.64.0.0/16"]
+  }
+}
+
+resource "aws_security_group_rule" "admgmt_allow" {
+  for_each          = var.admgmt_ingress_ports
+  type              = "ingress"
+  from_port         = each.key
+  to_port           = each.key
+  protocol          = "tcp"
+  cidr_blocks       = each.value
+  security_group_id = aws_security_group.admgmt.id
+}
+
+resource "aws_security_group_rule" "admgmt_outbound_all" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "all"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.admgmt.id
+}
+
+resource "aws_security_group_rule" "admgmt_allow_icmp" {
+  type              = "ingress"
+  from_port         = "-1"
+  to_port           = "-1"
+  protocol          = "icmp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.admgmt.id
+}
+
+
+## Rule for directory service
+
+resource "aws_security_group_rule" "dns_outbound_udp" {
+  type              = "egress"
+  from_port         = 53
+  to_port           = 53
+  protocol          = "udp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_directory_service_directory.a1.security_group_id
+}
+
+resource "aws_security_group_rule" "dns_outbound_tcp" {
+  type              = "egress"
+  from_port         = 53
+  to_port           = 53
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_directory_service_directory.a1.security_group_id
+}
+
+resource "aws_security_group_rule" "ad_trust_outbound" {
+  type              = "egress"
+  from_port         = 0
+  to_port           = 0
+  protocol          = "-1"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_directory_service_directory.a1.security_group_id
+}
