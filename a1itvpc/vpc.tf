a571bb169866cf75d43e0dce0a07b4f0e5ef87ac
kickchong <kickchong@gmail.com>
2022-09-20 08:28:48 -0700

    t2

--- a1itvpc/vpc.tf
+++ a1itvpc/vpc.tf
@@ -1,7 +1,7 @@
-output "zone" {
-    //value = "${length(data.aws_availability_zones.westzone.names)}"
-  value = "${element(data.aws_availability_zones.westzone.names,1)}"
-}
+# output "zone" {
+#     //value = "${length(data.aws_availability_zones.westzone.names)}"
+#   value = "${element(data.aws_availability_zones.westzone.names,1)}"
+# }
 
 resource "aws_vpc" "v21qw1" {
     provider = aws.us-west-1
@@ -22,28 +22,28 @@ resource "aws_vpc" "v21qe1" {
     }
 }
 
-resource "aws_subnet" "westwebsub" {
-    count = "${length(data.aws_availability_zones.westzone.names)}"
-    vpc_id = aws_vpc.v21qw1.id
-    provider = aws.us-west-1
-    cidr_block = "10.64.${count.index}.0/24"
-    map_public_ip_on_launch = "true"
-    availability_zone = "${element(data.aws_availability_zones.westzone.names,count.index)}"
+# resource "aws_subnet" "westwebsub" {
+#     count = "${length(data.aws_availability_zones.westzone.names)}"
+#     vpc_id = aws_vpc.v21qw1.id
+#     provider = aws.us-west-1
+#     cidr_block = "10.64.${count.index}.0/24"
+#     map_public_ip_on_launch = "true"
+#     availability_zone = "${element(data.aws_availability_zones.westzone.names,count.index)}"
 
-    tags = {
-         Name = "public-${element(data.aws_availability_zones.westzone.names, count.index)}"
-    }
-}
+#     tags = {
+#          Name = "public-${element(data.aws_availability_zones.westzone.names, count.index)}"
+#     }
+# }
 
-resource "aws_subnet" "eastwebsub" {
-    count = "${length(data.aws_availability_zones.eastzone.names)}"
-    vpc_id = aws_vpc.v21qe1.id
-    provider = aws.us-east-1
-    cidr_block = "10.128.${count.index}.0/24"
-    map_public_ip_on_launch = "true"
-    availability_zone = "${element(data.aws_availability_zones.eastzone.names,count.index)}"
+# resource "aws_subnet" "eastwebsub" {
+#     count = "${length(data.aws_availability_zones.eastzone.names)}"
+#     vpc_id = aws_vpc.v21qe1.id
+#     provider = aws.us-east-1
+#     cidr_block = "10.128.${count.index}.0/24"
+#     map_public_ip_on_launch = "true"
+#     availability_zone = "${element(data.aws_availability_zones.eastzone.names,count.index)}"
 
-    tags = {
-         Name = "public-${element(data.aws_availability_zones.eastzone.names, count.index)}"
-    }
-}
+#     tags = {
+#          Name = "public-${element(data.aws_availability_zones.eastzone.names, count.index)}"
+#     }
+# }
