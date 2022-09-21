d4683e58f3bd88df94241661f46d95d67f0bd60b
kickchong <kickchong@gmail.com>
2022-09-21 08:56:33 -0700

    r

--- a1itvpc/variables.tf
+++ a1itvpc/variables.tf
@@ -6,23 +6,23 @@ data "aws_availability_zones" "eastzone" {
     provider = aws.us-east
 }
 
-variable "domain_password" {
-  description = "Domain Password"
-  type        = string
-}
+# variable "domain_password" {
+#   description = "Domain Password"
+#   type        = string
+# }
 
-variable "domain_name" {
-  description = "Domain Name"
-  type        = string
-}
+# variable "domain_name" {
+#   description = "Domain Name"
+#   type        = string
+# }
 
-variable "condfwdip" {
-  description = "Conditional Forward IP for local R53 zone"
-  type        = string
-}
+# variable "condfwdip" {
+#   description = "Conditional Forward IP for local R53 zone"
+#   type        = string
+# }
 
-variable "dnszone" {
-  description = "Local 53 DNS domain name"
-  type        = string
-}
+# variable "dnszone" {
+#   description = "Local 53 DNS domain name"
+#   type        = string
+# }
 
