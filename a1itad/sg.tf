5b58ca16c851aac56b38dbbe519470a6415d0fee
kickchong <kickchong@gmail.com>
2022-09-22 15:16:26 -0700

    remove ad/d

--- a1itad/sg.tf
+++ a1itad/sg.tf
@@ -1,26 +1,26 @@
-resource "aws_security_group_rule" "dns_outbound_udp" {
-  type              = "egress"
-  from_port         = 53
-  to_port           = 53
-  protocol          = "udp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_directory_service_directory.a1.security_group_id
-}
+# resource "aws_security_group_rule" "dns_outbound_udp" {
+#   type              = "egress"
+#   from_port         = 53
+#   to_port           = 53
+#   protocol          = "udp"
+#   cidr_blocks       = ["0.0.0.0/0"]
+#   security_group_id = aws_directory_service_directory.a1.security_group_id
+# }
 
-resource "aws_security_group_rule" "dns_outbound_tcp" {
-  type              = "egress"
-  from_port         = 53
-  to_port           = 53
-  protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_directory_service_directory.a1.security_group_id
-}
+# resource "aws_security_group_rule" "dns_outbound_tcp" {
+#   type              = "egress"
+#   from_port         = 53
+#   to_port           = 53
+#   protocol          = "tcp"
+#   cidr_blocks       = ["0.0.0.0/0"]
+#   security_group_id = aws_directory_service_directory.a1.security_group_id
+# }
 
-resource "aws_security_group_rule" "ad_trust_outbound" {
-  type              = "egress"
-  from_port         = 0
-  to_port           = 0
-  protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
-  security_group_id = aws_directory_service_directory.a1.security_group_id
-}
+# resource "aws_security_group_rule" "ad_trust_outbound" {
+#   type              = "egress"
+#   from_port         = 0
+#   to_port           = 0
+#   protocol          = "-1"
+#   cidr_blocks       = ["0.0.0.0/0"]
+#   security_group_id = aws_directory_service_directory.a1.security_group_id
+# }
