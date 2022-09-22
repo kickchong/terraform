1cb3f9af89c9fd213beaf2e4f9ec5fcd548270e3
kickchong <kickchong@gmail.com>
2022-09-22 09:23:22 -0700

    add tags

--- a1itvpc/provider.tf
+++ a1itvpc/provider.tf
@@ -15,6 +15,14 @@ provider "aws" {
 provider "aws" {
   region     = "us-west-2"
   alias      = "us-west"
+  default_tags {
+    tags = {
+      Managedby               = "Albert Chong"  
+ #     Repo-Branch             = var.repo_branch
+      TerraformWorkspace      = terraform.workspace
+    }
+  }
+
 }
 provider "aws" {
   region     = "us-east-1"
