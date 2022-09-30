8d80d206db6e0743e257d510d9bde1747f0c0a5d
kickchong <kickchong@gmail.com>
2022-09-30 10:20:20 -0700

    u 1

--- a1itad/ad.tf
+++ a1itad/ad.tf
@@ -2,11 +2,9 @@ resource "aws_directory_service_directory" "a1" {
   name        = var.domain_name
   password    = var.domain_password
   type        = "MicrosoftAD"
-#  size        = "Small"
   description = "Managed by terraform.io"
   vpc_settings {
     vpc_id     = data.aws_vpc.v21qw1.id
-#    subnet_ids = [data.aws_subnet.westwebsub[0].id]
     subnet_ids = [data.aws_subnet.westwebsub[0].id,data.aws_subnet.westwebsub[1].id]
 
   }
