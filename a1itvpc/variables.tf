fc915cb091650f2cadc04d283ad3653d608d320f
kickchong <kickchong@gmail.com>
2022-10-04 15:03:23 -0700

    add vpc

--- a1itvpc/variables.tf
+++ a1itvpc/variables.tf
@@ -6,6 +6,24 @@ data "aws_availability_zones" "eastzone" {
     provider = aws.us-east
 }
 
+variable "accounts" {
+  description = "List of AWS Accounts to target"
+  default = {
+    "albert" = {
+      env           = "a1"
+      cidr_block_w  = "10.64.0.0/16"
+      cidr_block_e  = "10.65.0.0/16"
+      r53branch     = "Infrastructure-Route53"
+    },
+    "kick" = {
+      env           = "k1"
+      cidr_block_w  = "10.80.0.0/16"
+      cidr_block_e  = "10.81.0.0/16"
+      r53branch     = "Infrastructure-Route53"
+    }  
+  }
+}
+
 # variable "domain_password" {
 #   description = "Domain Password"
 #   type        = string
--- a1itvpc/vpc.tf
+++ a1itvpc/vpc.tf
@@ -1,33 +1,51 @@
-# # output "zone" {
-# #     //value = "${length(data.aws_availability_zones.westzone.names)}"
-# #   value = "${element(data.aws_availability_zones.westzone.names,1)}"
-# # }
+# output "zone" {
+#     //value = "${length(data.aws_availability_zones.westzone.names)}"
+#   value = "${element(data.aws_availability_zones.westzone.names,1)}"
+# }
 
-# resource "aws_vpc" "v21qw1" {
-#     cidr_block = "10.64.0.0/16"
-#     enable_dns_support = "true"
-#     enable_dns_hostnames = "true"
-#     tags = {
-#          Name = "v21qw1"
-#     }
-# }
-# resource "aws_vpc" "v21qe1" {
-#     provider = aws.us-east
-#     cidr_block = "10.128.0.0/16"
-#     enable_dns_support = "true"
-#     enable_dns_hostnames = "true"
-#     tags = {
-#          Name = "v21qe1"
-#     }
-# }
+resource "aws_vpc" "v21qw1" {
+    for_each                = var.accounts
+    cidr_block              = each.value.cidr_block_w
+    enable_dns_support      = "true"
+    enable_dns_hostnames    = "true"
+    tags = {
+         Name = "v21qw1"
+    }
+}
+resource "aws_vpc" "v21qe1" {
+    for_each                = var.accounts
+    provider                = aws.us-east
+    cidr_block              = each.value.cidr_block_e
+    enable_dns_support      = "true"
+    enable_dns_hostnames    = "true"
+    tags = {
+         Name = "v21qe1"
+    }
+}
 
 # resource "aws_subnet" "westwebsub" {
-#     count = "${length(data.aws_availability_zones.westzone.names)}"
-#     vpc_id = aws_vpc.v21qw1.id
-#     provider = aws.us-west
-#     cidr_block = "10.64.${count.index}.0/24"
+#     for_each                = var.accounts  == "albert" || var.accounts == "kick" ? 1 : 0
+#     count                   = "${length(data.aws_availability_zones.westzone.names)}"
+#     vpc_id                  = aws_vpc.v21qw1[each.key].id
+#     provider                = aws.us-west
+#     cidr_block              = "10.64.${count.index}.0/24"
 #     map_public_ip_on_launch = "true"
-#     availability_zone = "${element(data.aws_availability_zones.westzone.names,count.index)}"
+#     availability_zone       = "${element(data.aws_availability_zones.westzone.names,count.index)}"
+
+#     tags = {
+#          Name = "public-${element(data.aws_availability_zones.westzone.names, count.index)}"
+#     }
+# }
+
+
+# resource "aws_subnet" "westwebsub2" {
+#     for_each                = var.accounts  == "albert" || var.accounts == "kick" ? 0 : 1
+#     count                   = "${length(data.aws_availability_zones.westzone.names)}"
+#     vpc_id                  = aws_vpc.v21qw1[each.key].id
+#     provider                = aws.us-west
+#     cidr_block              = "10.80.${count.index}.0/24"
+#     map_public_ip_on_launch = "true"
+#     availability_zone       = "${element(data.aws_availability_zones.westzone.names,count.index)}"
 
 #     tags = {
 #          Name = "public-${element(data.aws_availability_zones.westzone.names, count.index)}"
@@ -35,6 +53,7 @@
 # }
 
 # resource "aws_subnet" "eastwebsub" {
+#     for_each                = var.accounts  == "albert" || var.accounts == "kick" ? 1 : 0
 #     count = "${length(data.aws_availability_zones.eastzone.names)}"
 #     vpc_id = aws_vpc.v21qe1.id
 #     provider = aws.us-east
