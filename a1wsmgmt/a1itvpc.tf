62e5e0797c1ba3d840080d77c2c117bcf1b85714
kickchong <kickchong@gmail.com>
2022-10-05 11:56:07 -0700

    u

--- a1wsmgmt/a1itvpc.tf
+++ a1wsmgmt/a1itvpc.tf
@@ -1,12 +1,11 @@
 resource "tfe_workspace" "a1_infrastructure_vpc" {
-  for_each          = var.accounts
-  name              = "${each.value.env}_Infrastructure_VPC"
+  name              = "a1_Infrastructure_VPC"
   organization      = "albertchong"
   execution_mode    = "remote"
   working_directory = "a1itvpc"
   auto_apply        = "false"
   queue_all_runs    = "false"
-  tag_names         = ["account:${each.key}", "vpc", "subnet", "sg"] 
+  tag_names         = ["account:a1", "vpc", "subnet", "sg"] 
   vcs_repo {
     identifier     = "kickchong/terraform"
     branch         = "a1itvpc"
@@ -15,31 +14,30 @@ resource "tfe_workspace" "a1_infrastructure_vpc" {
   }
 }
 
-resource "tfe_variable" "infrastructure_env" {
-  for_each     = var.accounts
-  key          = "env"
-  value        = each.key
-  category     = "terraform"
-  workspace_id = tfe_workspace.a1_infrastructure_vpc[each.key].id
-  description  = "Environment variable used by Terraform Code"
-}
-
 resource "tfe_variable" "a1_vpc_aws_access_key" {
-  for_each     = var.accounts
   key          = "AWS_ACCESS_KEY_ID"
-  value        = data.aws_ssm_parameters_by_path.tf_common_wsmgmt.values[index(data.aws_ssm_parameters_by_path.tf_common_wsmgmt.names, "${var.ssmpath}/common/${each.value.env}_access_key")]  
+  value        = data.aws_ssm_parameters_by_path.tf_common_wsmgmt.values[index(data.aws_ssm_parameters_by_path.tf_common_wsmgmt.names, "${var.ssmpath}/common/a1_access_key")]  
   category     = "env"
   sensitive    = true
-  workspace_id = tfe_workspace.a1_infrastructure_vpc[each.key].id
+  workspace_id = tfe_workspace.a1_infrastructure_vpc.id
   description  = "Access Key used by Terraform Code"
 }
 
 resource "tfe_variable" "a1_vpc_secret_key" {
-  for_each     = var.accounts
   key          = "AWS_SECRET_ACCESS_KEY"
-  value        = data.aws_ssm_parameters_by_path.tf_common_wsmgmt.values[index(data.aws_ssm_parameters_by_path.tf_common_wsmgmt.names, "${var.ssmpath}/common/${each.value.env}_secret_key")]
+  value        = data.aws_ssm_parameters_by_path.tf_common_wsmgmt.values[index(data.aws_ssm_parameters_by_path.tf_common_wsmgmt.names, "${var.ssmpath}/common/a1_secret_key")]
   category     = "env"
   sensitive    = true
-  workspace_id = tfe_workspace.a1_infrastructure_vpc[each.key].id
+  workspace_id = tfe_workspace.a1_infrastructure_vpc.id
   description  = "Secret Key used by Terraform Code"
 }
+
+
+
+# resource "tfe_variable" "infrastructure_env" {
+#   key          = "env"
+#   value        = each.key
+#   category     = "terraform"
+#   workspace_id = tfe_workspace.a1_infrastructure_vpc.id
+#   description  = "Environment variable used by Terraform Code"
+# }
