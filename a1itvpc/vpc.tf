4861fcac04c2e3da50ae633d36e7457b4f9b58e6
kickchong <kickchong@gmail.com>
2022-10-05 11:50:11 -0700

    rollback

--- a1itvpc/vpc.tf
+++ a1itvpc/vpc.tf
@@ -1,46 +1,46 @@
-# output "zone" {
-#     //value = "${length(data.aws_availability_zones.westzone.names)}"
-#   value = "${element(data.aws_availability_zones.westzone.names,1)}"
+# # output "zone" {
+# #     //value = "${length(data.aws_availability_zones.westzone.names)}"
+# #   value = "${element(data.aws_availability_zones.westzone.names,1)}"
+# # }
+
+# resource "aws_vpc" "v21qw1" {
+#     count                   = var.env == "albert" || var.accounts == "kick" ? 1 : 0
+#     cidr_block              = "10.64.0.0/16"
+#     enable_dns_support      = "true"
+#     enable_dns_hostnames    = "true"
+#     tags = {
+#          Name = "v21qw1"
+#     }
+# }
+# resource "aws_vpc" "v21qe1" {
+#     count                   = var.env == "albert" || var.accounts == "kick" ? 1 : 0
+#     provider                = aws.us-east
+#     cidr_block              = "10.80.0.0/16"
+#     enable_dns_support      = "true"
+#     enable_dns_hostnames    = "true"
+#     tags = {
+#          Name = "v21qe1"
+#     }
 # }
 
-resource "aws_vpc" "v21qw1" {
-    count                   = var.env == "albert" || var.accounts == "kick" ? 1 : 0
-    cidr_block              = "10.64.0.0/16"
-    enable_dns_support      = "true"
-    enable_dns_hostnames    = "true"
-    tags = {
-         Name = "v21qw1"
-    }
-}
-resource "aws_vpc" "v21qe1" {
-    count                   = var.env == "albert" || var.accounts == "kick" ? 1 : 0
-    provider                = aws.us-east
-    cidr_block              = "10.80.0.0/16"
-    enable_dns_support      = "true"
-    enable_dns_hostnames    = "true"
-    tags = {
-         Name = "v21qe1"
-    }
-}
-
-resource "aws_subnet" "westwebsub1" {
-    count                   = var.accounts  == "albert" || var.accounts == "kick" ? 1 : 0
-    vpc_id                  = aws_vpc.v21qw1[0].id
-    cidr_block              = "10.64.0.0/24"
-    map_public_ip_on_launch = "true"
-    tags = {
-         Name = "public-westwebsub1"
-    }
-}
-resource "aws_subnet" "westwebsub2" {
-    count                   = var.accounts  == "albert" || var.accounts == "kick" ? 1 : 0
-    vpc_id                  = aws_vpc.v21qe1[0].id
-    cidr_block              = "10.80.0.0/24"
-    map_public_ip_on_launch = "true"
-    tags = {
-         Name = "public-westwebsub2"
-    }
-}
+# resource "aws_subnet" "westwebsub1" {
+#     count                   = var.accounts  == "albert" || var.accounts == "kick" ? 1 : 0
+#     vpc_id                  = aws_vpc.v21qw1[0].id
+#     cidr_block              = "10.64.0.0/24"
+#     map_public_ip_on_launch = "true"
+#     tags = {
+#          Name = "public-westwebsub1"
+#     }
+# }
+# resource "aws_subnet" "westwebsub2" {
+#     count                   = var.accounts  == "albert" || var.accounts == "kick" ? 1 : 0
+#     vpc_id                  = aws_vpc.v21qw1[0].id
+#     cidr_block              = "10.80.0.0/24"
+#     map_public_ip_on_launch = "true"
+#     tags = {
+#          Name = "public-westwebsub2"
+#     }
+# }
 
 
 
