d4831e100d5d32029b04d68dedad9bccd7b4813a
kickchong <kickchong@gmail.com>
2022-10-06 10:57:44 -0700

    c project

---
+++ a1wsmgmt/k1itproject.tf
@@ -0,0 +1,79 @@
+resource "tfe_workspace" "k1_project" {
+  name              = "k1_project"
+  organization      = "albertchong"
+  execution_mode    = "remote"
+  working_directory = "k1itproject"
+  auto_apply        = "false"
+  queue_all_runs    = "false"
+  tag_names         = ["account:k1", "project"]
+  vcs_repo {
+    identifier     = "kickchong/terraform"
+    branch         = "k1itproject"
+    oauth_token_id = data.aws_ssm_parameters_by_path.tf_common_wsmgmt.values[index(data.aws_ssm_parameters_by_path.tf_common_wsmgmt.names,"${var.ssmpath}/common/oauth_token_id")]
+  }
+}
+
+resource "tfe_variable" "k1_project_aws_access_key" {
+  key          = "AWS_ACCESS_KEY_ID"
+  value        = data.aws_ssm_parameters_by_path.tf_common_wsmgmt.values[index(data.aws_ssm_parameters_by_path.tf_common_wsmgmt.names, "${var.ssmpath}/common/k1_access_key")]  
+  category     = "env"
+  sensitive    = true
+  workspace_id = tfe_workspace.k1_project.id
+  description  = "Access Key used by Terraform Code"
+}
+
+resource "tfe_variable" "k1_project_secret_key" {
+  key          = "AWS_SECRET_ACCESS_KEY"
+  value        = data.aws_ssm_parameters_by_path.tf_common_wsmgmt.values[index(data.aws_ssm_parameters_by_path.tf_common_wsmgmt.names, "${var.ssmpath}/common/k1_secret_key")]
+  category     = "env"
+  sensitive    = true
+  workspace_id = tfe_workspace.k1_project.id
+  description  = "Secret Key used by Terraform Code"
+}
+
+# ## Create Space for AD
+# variable "ad_domain_k1" {
+#   default = {
+#     addomain  = "k1.chonglife.com"
+#     condfwdip = "10.128.0.2"
+#     dnszone   = "k1.chonglife.com"
+#   }
+# }
+
+# # variable "domain_password" {
+# #   description = "Environment Variable for example dev02, stage02, prod02"
+# #   type        = string
+# # }
+
+# resource "tfe_variable" "infrastructure_ad_domain_name_k1" {
+#   key          = "domain_name"
+#   value        = var.ad_domain.addomain
+#   category     = "terraform"
+#   workspace_id = tfe_workspace.k1_infrastructure_ad.id
+#   description  = "Domain Name for Directory Service"
+# }
+
+# resource "tfe_variable" "infrastructure_ad_domain_password_k1" {
+#   key          = "domain_password"
+#   value        = var.domain_password
+#   category     = "terraform"
+#   sensitive    = true
+#   workspace_id = tfe_workspace.k1_infrastructure_ad.id
+#   description  = "Domain Password for Directory Service"
+# }
+
+# resource "tfe_variable" "infrastructure_ad_domain_cndfwdip_k1" {
+#   key          = "condfwdip"
+#   value        = var.ad_domain.condfwdip
+#   category     = "terraform"
+#   workspace_id = tfe_workspace.k1_infrastructure_ad.id
+#   description  = "Domain Name for Directory Service"
+# }
+
+# resource "tfe_variable" "infrastructure_ad_domain_dnszone_k1" {
+#   key          = "dnszone"
+#   value        = var.ad_domain.dnszone
+#   category     = "terraform"
+#   workspace_id = tfe_workspace.k1_infrastructure_ad.id
+#   description  = "Domain Name for Directory Service"
+# }
