89795ad647832f3428e86ab635c2dd7708ea6c94
kickchong <kickchong@gmail.com>
2022-10-17 12:37:36 -0700

    c s3

--- k1itproject/z1lambda-s3-iam.tf
+++ k1itproject/z1lambda-s3-iam.tf
@@ -1,3 +1,21 @@
+resource "aws_s3_bucket" "chonglife" {
+    provider = aws.us-west
+    bucket = "www.${var.bucket_name}"
+ #   acl = "public-read"
+
+    website {
+      index_document = "index.html"
+      error_document = "error.html"
+    }
+  
+}
+
+resource "aws_s3_bucket_acl" "chonglife" {
+  bucket = aws_s3_bucket.chonglife.id
+  acl    = "public-read"
+}
+
+
 data "archive_file" "lambda_hello_world" {
     type = "zip"
     source_dir = "${path.module}/hwjs"
